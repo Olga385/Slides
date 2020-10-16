70aca1a89f30b27d2b6659e9c259e28dc96809a6
olga <helga.88@inbox.ru>
2020-10-16 16:25:49 +0300

    added click

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let btnNext = document.getElementById("btnNext");
 let slides = document.querySelectorAll("img");
 let i = 0;
+
+btnNext.addEventListener("click", function () {
+    ++i;
+    if (i >= slides.length) {
+        slides[i - 1].classList.remove("block");
+        i = 0;
+        slides[i].classList.add("block");
+    } else {
+        slides[i - 1].classList.remove("block");
+        slides[i].classList.add("block");
+    }
+});
